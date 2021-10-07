560b1e655950ecf2154fdf3c7dceb51475fd7695
Louis Ouellet <louis@TheMacSwitch.local>
2021-10-07 14:30:30 -0400

    21.10-dev-7

--- dist/js/script.js
+++ dist/js/script.js
@@ -861,6 +861,8 @@ API.Plugins.calls = {
 						callUpdate.form = form;
 						API.request('calls','end',{ data:callUpdate },function(result){
 							var data = JSON.parse(result);
+							console.log(result);
+							console.log(data);
 							if(data.success != undefined){
 								// Update Call Window
 								if(callCTN.length > 0){
