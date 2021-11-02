185ebe42a45c1f20f772f83cb92c236a9a5e451e
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-02 11:17:00 -0400

    21.11-dev-75

--- dist/js/script.js
+++ dist/js/script.js
@@ -63,9 +63,9 @@ API.Plugins.calls = {
 			create:function(title,body,options = {},callback = null){
 				if(options instanceof Function){ callback = options; options = {}; }
 				var html = '';
-				html += '<div class="toast bg-dark fade show" style="width:100%;">';
-			    html += '<div class="toast-header" style="display:block;width:100%;">'+title+'</div>';
-			    html += '<div class="toast-body" style="width:100%;">'+body+'</div>';
+				html += '<div class="toast bg-dark mx-0 fade show" style="width:100%;">';
+			    html += '<div class="toast-header" style="display:block">'+title+'</div>';
+			    html += '<div class="toast-body">'+body+'</div>';
 			  html += '</div>';
 				API.Plugins.calls.GUI.toast.element.prepend(html);
 				var toast = API.Plugins.calls.GUI.toast.element.find('div.toast').first();
