ff5898dc164f56ba92d60f117a5475d0f5d14328
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-04 19:11:30 -0400

    21.11-dev-124

--- api.php
+++ api.php
@@ -76,151 +76,62 @@ class callsAPI extends CRUDAPI {
 	public function end($request = null, $data = null){
 		if(isset($data)){
 			if(!is_array($data)){ $data = json_decode($data, true); }
+			$return['request'] = $request;
+			$return['data'] = $data;
 			$this->Auth->setLimit(0);
 			$call = $this->Auth->read('calls',$data['id']);
 			if($call != null){
 				$call = $call->all()[0];
-				// $callRelationships = $this->getRelationships($request,$data['id']);
-				// $issues = [];
-				// foreach($callRelationships as $id => $relationships){
-				// 	foreach($relationships as $relationship){
-				// 		if($relationship['relationship'] == 'organizations'){ $organizationID = $relationship['link_to']; }
-				// 		if($relationship['relationship'] == 'issues'){ $issues[$relationship['link_to']] = $relationship['statuses']; }
-				// 	}
-				// }
-				// if(isset($data['form']['date'],$data['form']['time'])){ $call['status'] = 4; } else { $call['status'] = 5; }
-				// // Update Current Call
-				// $return = parent::update($request, $call);
-				// $return['data'] = $data;
-				// // Get Status
-				// $status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','calls',$call['status'])->fetchAll()->all()[0];
-				// $this->createRelationship([
-				// 	'relationship_1' => 'organizations',
-				// 	'link_to_1' => $organizationID,
-				// 	'relationship_2' => $request,
-				// 	'link_to_2' => $call['id'],
-				// 	'relationship_3' => 'statuses',
-				// 	'link_to_3' => $status['id'],
-				// ]);
-				// // Fetch Linked Organization
-				// if(isset($organizationID)){
-				// 	$organization = $this->Auth->read('organizations',$organizationID);
-				// 	$organizationRelationships = [];
-				// 	if($organization != null){
-				// 		$organization = $organization->all()[0];
-				// 		$call['relationship'] = 'organizations';
-				// 		$call['link_to'] = $organizationID;
-				// 		$organizationRelationships = $this->getRelationships($request,$data['id']);
-				// 	}
-				// 	$services = [];
-				// 	$organizationIssues = [];
-				// 	foreach($organizationRelationships as $id => $relationships){
-				// 		foreach($relationships as $relationship){
-				// 			if($relationship['relationship'] == 'services'){ array_push($services,$relationship['link_to']); }
-				// 			if($relationship['relationship'] == 'issues'){ $organizationIssues[$relationship['link_to']] = $relationship['statuses']; }
-				// 		}
-				// 	}
-				// }
-				// // Update Issues
-				// if(isset($data['form']['issues'])){
-				// 	foreach($data['form']['issues'] as $issueID => $issueStatus){
-				// 		$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','issues',$issueStatus)->fetchAll()->all()[0];
-				// 		if($status['id'] != $issues[$issueID]){
-				// 			// Update Issue Status of Call
-				// 			$this->createRelationship([
-				// 				'relationship_1' => 'issues',
-				// 				'link_to_1' => $issueID,
-				// 				'relationship_2' => 'calls',
-				// 				'link_to_2' => $call['id'],
-				// 				'relationship_3' => 'statuses',
-				// 				'link_to_3' => $status['id'],
-				// 			]);
-				// 			$return['output']['issues'][$issueID] = $this->Auth->read('issues',$issueID)->all()[0];
-				// 			$return['output']['issues'][$issueID] = $this->convertToDOM($return['output']['issues'][$issueID]);
-				// 			$return['output']['issues'][$issueID]['status'] = $status;
-				// 			// Update Issue Status of Organization
-				// 			if((isset($organizationID))&&($status['id'] != $organizationIssues[$issueID])){
-				// 				$this->createRelationship([
-				// 					'relationship_1' => 'issues',
-				// 					'link_to_1' => $issueID,
-				// 					'relationship_2' => 'organizations',
-				// 					'link_to_2' => $organizationID,
-				// 					'relationship_3' => 'statuses',
-				// 					'link_to_3' => $status['id'],
-				// 				]);
-				// 			}
-				// 			// Verify Linked Services
-				// 			if($status['order'] >= 4){
-				// 				$issueRelationships = $this->getRelationships('issues',$issueID);
-				// 				foreach($issueRelationships as $relationshipID => $relationships){
-				// 					foreach($relationships as $relationship){
-				// 						if(($relationship['relationship'] == 'services')){
-				// 							// Adding new Service to Call
-				// 							$this->createRelationship([
-				// 								'relationship_1' => 'calls',
-				// 								'link_to_1' => $call['id'],
-				// 								'relationship_2' => 'services',
-				// 								'link_to_2' => $relationship['link_to'],
-				// 							]);
-				// 							// Adding new Service to Organization
-				// 							if((isset($organizationID))&&(!in_array($relationship['link_to'],$services))){
-				// 								$this->createRelationship([
-				// 									'relationship_1' => 'organizations',
-				// 									'link_to_1' => $organizationID,
-				// 									'relationship_2' => 'services',
-				// 									'link_to_2' => $relationship['link_to'],
-				// 								]);
-				// 								$service = $this->Auth->read('services',$relationship['link_to']);
-				// 								if($service != null){
-				// 									$return['output']['services'][$relationship['link_to']] = $service->all()[0];
-				// 									$return['output']['services'][$relationship['link_to']] = $this->convertToDOM($return['output']['services'][$relationship['link_to']]);
-				// 								}
-				// 								// Update isClient Switch
-				// 								$organization['isClient'] = 'true';
-				// 								$this->Auth->update('organizations',$organization,$organization['id']);
-				// 							}
-				// 						}
-				// 					}
-				// 				}
-				// 			}
-				// 		}
-				// 	}
-				// }
-				// // Create Note
-				// if(isset($data['form']['note'])){
-				// 	$note = [
-				// 		'by' => $this->Auth->User['id'],
-				// 		'content' => $data['form']['note'],
-				// 	];
-				// 	$noteID = $this->Auth->create('notes',$note);
-				// 	$return['output']['note']['raw'] = $this->Auth->read('notes',$noteID)->all()[0];
-				// 	$return['output']['note']['dom'] = $this->convertToDOM($return['output']['note']['raw']);
-				// }
-				// // Create Callback
-				// if($call['status'] == 4){
-				// 	$call['date'] = $data['form']['date'];
-				// 	$call['time'] = $data['form']['time'];
-				// 	$call['status'] = 1;
-				// 	unset($call['created']);
-				// 	$return['output']['new'] = $this->create($request, $call);
-				// }
-				// // Create Relationships
-				// $this->createRelationship([
-				// 	'relationship_1' => $request,
-				// 	'link_to_1' => $data['id'],
-				// 	'relationship_2' => 'notes',
-				// 	'link_to_2' => $noteID,
-				// ]);
-				// if(isset($organizationID)){
-				// 	$this->createRelationship([
-				// 		'relationship_1' => 'organizations',
-				// 		'link_to_1' => $organizationID,
-				// 		'relationship_2' => 'notes',
-				// 		'link_to_2' => $noteID,
-				// 	]);
-				// }
-				// // Return
-				// return $return;
+				$call['status'] = $data['status'];
+				$this->Auth->update('calls',$call,$call['id']);
+				$return['success'] = $this->Language->Field["Record successfully updated"];
+				$return['output']['dom'] = $this->convertToDOM($call);
+				$return['output']['raw'] = $call;
+				foreach($data['issues'] as $id => $status){
+					$return['output']['issues'][$id] = $this->Auth->read('issues',$id)->all()[0];
+					$status = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?','calls',$call['status'])->fetchAll()->all()[0];
+					$this->createRelationship([
+						'relationship_1' => 'issues',
+						'link_to_1' => $id,
+						'relationship_2' => 'organizations',
+						'link_to_2' => $call['organization'],
+						'relationship_3' => 'statuses',
+						'link_to_3' => $status['id'],
+					]);
+					$this->createRelationship([
+						'relationship_1' => 'issues',
+						'link_to_1' => $id,
+						'relationship_2' => 'calls',
+						'link_to_2' => $call['id'],
+						'relationship_3' => 'statuses',
+						'link_to_3' => $status['id'],
+					]);
+					$services = $this->Auth->query('SELECT * FROM `relationships` WHERE (`relationship_1` = ? AND `link_to_1` = ? AND `relationship_2` = ?) || (`relationship_2` = ? AND `link_to_2` = ? AND `relationship_1` = ?)','issues',$id,'services','issues',$id,'services');
+					if($services->numRows() > 0){
+						$services = $services->fetchAll()->all();
+						foreach($services as $service){
+							$return['output']['services'][$service['id']] = $service;
+							$this->createRelationship([
+								'relationship_1' => 'organizations',
+								'link_to_1' => $call['organization'],
+								'relationship_2' => 'services',
+								'link_to_2' => $service['id'],
+							]);
+							$this->createRelationship([
+								'relationship_1' => 'calls',
+								'link_to_1' => $call['id'],
+								'relationship_2' => 'services',
+								'link_to_2' => $service['id'],
+							]);
+						}
+					}
+				}
+				if($data['note']){
+					$return['output']['note'] = $this->note('calls',['content' => $data['note']]);
+				}
+				if($call['status'] == 4){ $return['output']['new'] = $this->create('calls',$call); }
+				// Return
+				return $return;
 			}
 		}
 	}
